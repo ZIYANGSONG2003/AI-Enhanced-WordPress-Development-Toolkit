cf62c01ec85fb54507bf8fe47bcd2d98d9543959
ZIYANG SONG <u7466422@anu.edu.au>
2024-08-04 21:46:56 +1000

    upload wordpress folder

---
+++ wordpress/wordpress_plugin/link_wp.bat
@@ -0,0 +1,1 @@
+ssh -i "C:\Users\bjsyb\.ssh\worthbuy.pem" ubuntu@13.236.176.215
---
+++ wordpress/wordpress_plugin/scp_putcode_ontheserver.bat
@@ -0,0 +1,25 @@
+scp -i "C:/Users/ZIYANG SONG/Desktop/worthbuy.pem" -r "C:/Users/ZIYANG SONG/Desktop/COMP3500_GITHUB_DESK/AI-Enhanced-WordPress-Development-Toolkit/wordpress/wordpress_plugin/syb_plugin.php" ubuntu@13.236.176.215:/var/www/html/wordpress/wp-content/plugins/syb_plugin/
+
+scp -i "C:/Users/ZIYANG SONG/Desktop/worthbuy.pem" -r "C:/aishare/sybwpplugins/syb_plugin/syb_plugin_js_function.js" ubuntu@13.236.176.215:/var/www/html/wordpress/wp-content/plugins/syb_plugin/
+
+scp -i "C:/Users/ZIYANG SONG/Desktop/worthbuy.pem" -r "C:/aishare/sybwpplugins/syb_plugin/syb_plugin_styles.css" ubuntu@13.236.176.215:/var/www/html/wordpress/wp-content/plugins/syb_plugin/
+
+scp -i "C:/Users/ZIYANG SONG/Desktop/worthbuy.pem" -r "C:/Users/ZIYANG SONG/Desktop/COMP3500_GITHUB_DESK/AI-Enhanced-WordPress-Development-Toolkit/wordpress/aishare0711GPTwpOKnodejs服务器程序/index.js" ubuntu@13.236.176.215:/var/www/html/wordpress/gptguider/
+
+scp -i "C:/Users/ZIYANG SONG/Desktop/worthbuy.pem" -r "C:/Users/ZIYANG SONG/Desktop/COMP3500_GITHUB_DESK/AI-Enhanced-WordPress-Development-Toolkit/wordpress/aishare0711GPTwpOKnodejs服务器程序/form.ejs" ubuntu@13.236.176.215:/var/www/html/wordpress/gptguider/
+
+scp -i "C:/Users/ZIYANG SONG/Desktop/worthbuy.pem" -r "C:/Users/ZIYANG SONG/Desktop/wordpress_backup/wordpress/wp-content/plugins/shortcode-visualizer/shortcode-visualizer.php" ubuntu@13.236.176.215:/var/www/html/wordpress/wp-content/plugins/
+
+
+scp -i "C:/Users/ZIYANG SONG/Desktop/worthbuy.pem" -r "C:/Users/ZIYANG SONG/Desktop/wordpress_backup/wordpress/wp-config.php" ubuntu@13.236.176.215:/var/www/html/wordpress/
+
+node /var/www/html/wordpress/gptguider/index.js
+scp -i "C:/Users/ZIYANG SONG/Desktop/worthbuy.pem" -r "C:\Users\ZIYANG SONG\Desktop\wordpress_backup\wordpress\wp-content\plugins\dynamic-shortcodes\dynamic-shortcodes.php" ubuntu@13.236.176.215:/var/www/html/wordpress/wp-content/plugins/
+
+scp -i "C:/Users/ZIYANG SONG/Desktop/worthbuy.pem" -r "C:\Users\ZIYANG SONG\Desktop\wordpress_backup\wordpress\wp-content\plugins\dynamic-shortcodes\dynamic-shortcodes.php" ubuntu@13.236.176.215:/tmp/
+
+ssh -i "C:/Users/ZIYANG SONG/Desktop/worthbuy.pem" ubuntu@13.236.176.215
+
+dynamic-shortcodes
+
+scp -i "C:/Users/ZIYANG SONG/Desktop/worthbuy.pem" -r "C:\Users\ZIYANG SONG\Desktop\wordpress_backup\wordpress\wp-content\plugins\dynamic-shortcodes\dynamic-shortcodes.php" ubuntu@13.236.176.215:/var/www/html/wordpress/wp-content/plugins/dynamic-shortcodes/
